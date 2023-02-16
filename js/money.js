43e35da8980009dd88b4f83276354e9b5c6e26c5
Tanvir Ahmed <tanvir.be.global@gmail.com>
2023-02-16 23:23:52 +0600

    complete money master

--- js/money.js
+++ js/money.js
@@ -6,6 +6,13 @@ function getInputValueById(inputId) {
     input.value = '';
     return inputValue;
 }
+// input value 
+function getIncomeInputValueById(incomeInputId) {
+    const incomeInput = document.getElementById(incomeInputId);
+    const incomeInputString = incomeInput.value;
+    const incomeInputValue = parseInt(incomeInputString);
+    return incomeInputValue;
+}
 
 // set innerText
 function setInnerText(elementId, value) {
@@ -15,7 +22,7 @@ function setInnerText(elementId, value) {
 
 // calculate btn  
 document.getElementById('calculate__btn').addEventListener('click', function () {
-    const incomeInput = getInputValueById('income__input');
+    const incomeInput = getIncomeInputValueById('income__input');
     const foodInput = getInputValueById('food__input');
     const rentInput = getInputValueById('rent__input');
     const clothesInput = getInputValueById('clothes__input');
@@ -36,26 +43,31 @@ document.getElementById('calculate__btn').addEventListener('click', function () 
     }
 })
 
-// save btn 
+// save btn
 document.getElementById('save__btn').addEventListener('click', function () {
-    const incomeInput = getInputValueById('income__input');
+    const incomeInput = getIncomeInputValueById('income__input');
     const saveInput = getInputValueById('save__input');
-    const saveAmount = (incomeInput / 100) * saveInput;
-    setInnerText('saving__amount', saveAmount);
-    // if (saveInput <= 100) {
-    //     const saveAmount = (incomeInput / 100) * saveInput;
-    //     setInnerText('saving__amount', saveAmount);
-    // }
-    // else if (!isNaN(saveInput)) {
-    //     alert('Please provide a valid input')
-    // }
-
+    // const saveAmount = (incomeInput / 100) * saveInput;
+    // setInnerText('saving__amount', saveAmount);
 
     const balance = document.getElementById('balance');
     const balanceString = balance.innerText;
     const balanceValue = parseInt(balanceString);
+    const saveAmount = (incomeInput / 100) * saveInput;
 
     const remainingBalance = balanceValue - saveAmount;
-    setInnerText('remaining__balance', remainingBalance);
+    if (saveInput <= 100) {
+        if (balanceValue <= 0) {
+            alert('I can not calculate becauese balance is Zero(0)')
+        }
+        else {
+            setInnerText('saving__amount', saveAmount);
+            setInnerText('remaining__balance', remainingBalance);
+        }
+    }
+
+    else {
+        alert('Please provide a valid input')
+    }
 
 })
